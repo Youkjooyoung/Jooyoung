477e5571cf4d76f507552156f615619b68a896ad
6joo <117325378+Youkjooyoung@users.noreply.github.com>
2025-10-08 07:58:09 +0900

    Delete 09.Model2MVCShop(jQuery) directory

--- 09.Model2MVCShop(jQuery)/src/main/webapp/javascript/productDetail.js
+++
@@ -1,8 +0,0 @@
-// /javascript/productDetail.js
-(function($, d) {
-	'use strict'; if (!$) return;
-
-	$(d).on('submit', '.delete-form', function(e) {
-		if (!confirm('정말 이 상품을 삭제하시겠습니까?')) e.preventDefault();
-	});
-})(jQuery, document);
