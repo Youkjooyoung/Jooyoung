e92a5f5513b7da0cb6b9db46e2b17e1fab252949
6joo <117325378+Youkjooyoung@users.noreply.github.com>
2025-11-01 16:57:41 +0900

    Delete 12Model2MVCShop_SpringBoot3.5 directory

--- 12Model2MVCShop_SpringBoot3.5/src/main/java/com/model2/mvc/service/kakao/KakaoOAuthService.java
+++
@@ -1,9 +0,0 @@
-package com.model2.mvc.service.kakao;
-
-import com.model2.mvc.service.domain.User;
-
-public interface KakaoOAuthService {
-	String getAccessToken(String code) throws Exception;
-
-	User getUserInfo(String accessToken) throws Exception;
-}
--- 12Model2MVCShop_SpringBoot3.5/src/main/java/com/model2/mvc/service/kakaopay/KakaoPayService.java
+++
@@ -1,9 +0,0 @@
-package com.model2.mvc.service.kakaopay;
-
-import java.util.Map;
-
-public interface KakaoPayService {
-	Map<String, Object> readyPayment(Map<String, Object> param) throws Exception;
-
-	Map<String, Object> approvePayment(Map<String, Object> param) throws Exception;
-}
